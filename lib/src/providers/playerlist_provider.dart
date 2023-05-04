f5bd8e3ac6c9b39515443d3ccea1d67f09e6e732
yukitake <koshi.yukitake@gmail.com>
2023-05-04 12:41:05 +0900

    フォーマット調整

--- lib/src/providers/playerlist_provider.dart
+++ lib/src/providers/playerlist_provider.dart
@@ -1,13 +1,19 @@
+import 'package:bb_stats/src/repositories/teamStatRepository.php.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../collections/player/player.dart';
 import '../collections/playerList/player_list_model.dart';
 import '../repositories/player_repository.dart';
 
 class PlayerListStateNotifier extends StateNotifier<PlayerListModel> {
-  PlayerListStateNotifier(this.playerRepository) : super (PlayerListModel(
-    players: playerRepository.getAllPlayers(true),
-    visible: true
-  ));
+  PlayerListStateNotifier(
+      this.playerRepository,
+  ) : super (
+      PlayerListModel(
+        players: playerRepository.getAllPlayers(true),
+        visible: true
+      )
+  );
+
   final PlayerRepository playerRepository;
 
   void resetPlayerListState() {
